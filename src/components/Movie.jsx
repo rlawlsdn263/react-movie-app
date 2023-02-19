b1c70cb495f928e9d4b263e69115dd7aee6d8af3
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-19 16:11:42 +0900

    [Add] Movie 컴포넌트 이미지 경로 데이터 바인딩

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// 이미지 절대경로 설정
+const IMG_BASE_URL = "https://image.tmdb.org/t/p/w1280/";
+
 /* 
   Movie 컴포넌트 생성
   props 파라미터 세팅
@@ -7,7 +10,8 @@ import React from 'react';
 function Movie({title, poster_path, vote_average }){
   return(
     <div className="movie-container">
-      <img />
+      {/* 이미지 경로 + props */}
+      <img src={IMG_BASE_URL + poster_path} alt="영화포스터"/>
       <div className="movie-info">
         <h4>제목</h4>
         <span>평점</span>
