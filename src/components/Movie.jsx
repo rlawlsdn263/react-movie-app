06ef29ae1bd2103519bcd806fe8628fcdf6058d4
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-19 16:08:35 +0900

    [Add] Movie 컴포넌트 JSX 뼈대 완성

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
-// Movie 컴포넌트 생성
-function Movie(){
+/* 
+  Movie 컴포넌트 생성
+  props 파라미터 세팅
+*/
+function Movie({title, poster_path, vote_average }){
   return(
-    <div>Movie</div>
+    <div className="movie-container">
+      <img />
+      <div className="movie-info">
+        <h4>제목</h4>
+        <span>평점</span>
+      </div>
+    </div>
   )
 }
 
