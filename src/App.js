dfd7ec2d51434f22b5f77d3f4b113591d76176b1
rlawlsdn263 <rlawlsdn263@naver.com>
2023-02-19 16:03:41 +0900

    [Add] dummy 파일 리스트렌더링

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { dummy } from "./movieDummy";
+import Movie from "./components/Movie";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="app-container">
+        {/* dummy 파일에 접근해 리스트 렌더링하기 */}
+        {dummy.results.map((item) => {
+          return <Movie></Movie>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
